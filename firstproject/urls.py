c118b92949f638f9ec46ede751e3455da1549fbf
mingyao808 <mingyao808@hotmail.com>
2017-12-04 12:45:33 +0800

    a

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -20,6 +20,6 @@ from myapp.views import sayhello,hello
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', sayhello),
-    url(r'^$', hello),
+    url(r'^hello/(\w+)/$', hello),
   
 ]
